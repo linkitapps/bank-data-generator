e04c26fade02c71cb2fccd183c8d003cf6e3a59d
linkitapps <master@linkit.kr>
2016-04-03 21:26:49 +0900

    update

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ https://github.com/electron-userland/electron-builder
 https://medium.com/@svilen/auto-updating-apps-for-windows-and-osx-using-electron-the-complete-guide-4aa7a50b904c#.citr7irfv
 electron-installer-windows
 electron-packager
+
+# TODO
+사용자가 업로드 한 엑셀 파일의 컬럼과 FinDash 가 필요로 하는 컬럼들 (일시, 기재내용, 받은금액, ...) 을 매칭시키는 UI 를 만들고 매칭 이후 엑셀 파일 다운로드 기능까지 만듬
+- 사용자의 은행 거래내역 엑셀파일을 FinDash 서버에 직접 업로드 하지 않게 하기 위해서 => 은행 파일을 직접 업로드 하지 않게해 조금이라도 보안성을 높임
+- 사용자가 직접 매칭을 시킴으로써 사용자는 조금 불편해지지만 FinDash 가 국내 은행들의 모든 엑셀 양식을 취합하지 않아도 모든 은행 거래내역을 분석할 수 있게 되므로 오히려 사용자에게 도움이 되는 면이 있음
+- 글로벌 서비스를 하려면 세계 모든 은행들에 대해 직접 대응할 수는 없으므로 이런 식의 접근이 필요할 것 같음
--- script.js
+++ script.js
@@ -3,6 +3,7 @@ require('jquery-csv');
 
 var X = XLSX;
 
+// fix data
 function fixdata(data) {
   var o = '',
     l = 0,
@@ -16,6 +17,7 @@ function fixdata(data) {
   return o;
 }
 
+// workbook 을 CSV 로 변환하는 함수
 function to_csv(workbook) {
   var result = [];
 
@@ -31,31 +33,54 @@ function to_csv(workbook) {
   return result.join("\n");
 }
 
+// workbook 처리
 function process_wb(wb) {
   var tbody = document.getElementById('out-tbody'),
     html = '',
-    output = '';
-  output = to_csv(wb);
+    output = to_csv(wb);
 
-  // if(out.innerText === undefined) {
-  //   out.textContent = output;
-  // } else {
-  //   out.innerText = output;
-  // }
-
-  var data = jQuery.csv.toArrays(output);
+  var data = jQuery.csv.toArrays(output),
+    itemLen = data[0].length,
+    j,
+    start = false,
+    thead = false,
+    tag;
   jQuery.each(data, function(i, item) {
-    html += '<tr>';
-    html += '<td>' + item[0] + '</td>';
-    html += '<td>' + item[1] + '</td>';
-    html += '<td>' + item[2] + '</td>';
-    html += '<td>' + item[3] + '</td>';
-    html += '<td>' + item[4] + '</td>';
-    html += '<td>' + item[5] + '</td>';
-    html += '<td>' + item[6] + '</td>';
-    html += '<td>' + item[7] + '</td>';
-    html += '<td>' + item[8] + '</td>';
-    html += '</tr>';
+    // 맨 앞 컬럼 값이 없으면 유효하지 않은 row 로 판단함: 예) 우리은행 개인뱅킹 맨 아랫줄에 나오는 합계 row
+    if (!item[0]) {
+      return;
+    }
+
+    if (!start) {
+      var flag = true;
+      for (j = 0; j < itemLen; j++) {
+        if (!item[j]) {
+          flag = false;
+        }
+      }
+      if (flag) {
+        start = true;
+        thead = true;
+      }
+    }
+
+    // 엑셀 파일에서 모든 컬럼에 값이 있는 경우 start = true
+    // 이런 경우는 엑셀 파일 상단에 있는 요약 정보가 끝나고 실제로 내용이 시작되는 부분임 => 표의 header
+    // 이때 부터 테이블 생성을 시작함
+    if (start) {
+      if (thead) {
+        tag = 'th';
+        thead = false;
+      } else {
+        tag = 'td';
+      }
+
+      html += '<tr>';
+      for (j = 0; j < itemLen; j++) {
+        html += '<' + tag + '>' + item[j] + '</' + tag + '>';
+      }
+      html += '</tr>';
+    }
   });
   tbody.innerHTML = html;
 
@@ -64,14 +89,13 @@ function process_wb(wb) {
   }
 }
 
+// 엑셀 파일 선택 이벤트 핸들러
 function handleFile(e) {
   var files = e.target.files;
   var f = files[0];
   var reader = new FileReader();
-  var name = f.name;
-  // if(typeof console !== 'undefined') {
-  //   console.log(name);
-  // }
+  //var name = f.name;    // 파일 이름
+
   reader.onload = function(e) {
     var data = e.target.result;
     var wb;
@@ -82,8 +106,10 @@ function handleFile(e) {
   reader.readAsArrayBuffer(f);
 }
 
+// 엑셀 파일 선택
 document.getElementById('xlfile').addEventListener('change', handleFile, false);
 
+// 테이블에 로드된 데이터들을 엑셀 파일로 export
 function exportExcel() {
   if (!document.getElementById('out-tbody').innerHTML) {
     alert('먼저 엑셀파일을 업로드하십시오');
